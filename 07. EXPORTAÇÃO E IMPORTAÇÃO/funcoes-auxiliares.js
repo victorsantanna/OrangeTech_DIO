ccc5e11fc718ade178b7afea3290006767980643
Victor Santana <victor.esef@hotmail.com>
2022-11-28 11:41:45 -0300

    Aula sobre JavaScript

--- 07. EXPORTAÇÃO E IMPORTAÇÃO/funcoes-auxiliares.js
+++ 07. EXPORTAÇÃO E IMPORTAÇÃO/funcoes-auxiliares.js
@@ -1,7 +1,10 @@
-//exemplo1
+const entrada = [4, 3, 50, 10, 918,23, 1500];
+let i = 0;
 
 function gets(){
-    return 10;
+    const valor = entrada[i];
+    i++;
+    return valor;
 }
 
 function print(texto){
--- 07. EXPORTAÇÃO E IMPORTAÇÃO/main.js
+++ 07. EXPORTAÇÃO E IMPORTAÇÃO/main.js
@@ -1,6 +1,48 @@
-const funcoes = require('./funcoes-auxiliares');
+/*
+Uma sala contem 5 alunos e para cada aluno foi sorteado um numero de 1 a 100.
+faça um programa que receba os 5 numeros sorteados para os alunos e mostre o maior numero sorteado.
 
-console.log(funcoes.gets());
+dado de entrada: 5, 50, 10, 98,23
+saída: 98
+*/
 
-//exemplo
+const {gets, print} = require('./funcoes-auxiliares');
 
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++){
+    const numeroSorteados = gets();
+    numerosSorteados.push(numeroSorteados);
+} 
+
+let maiorValor = 0;
+for (let i = 0; i < numerosSorteados.length; i++){
+    const numeroSorteados = numerosSorteados[i];
+    if ( numeroSorteados > maiorValor){
+        maiorValor = numeroSorteados;
+    }
+}
+
+
+let maiorValorEncontrado = 0;
+
+for (let i = 0; i < 5; i++){
+    const numeroSorteados = gets();
+    if(numeroSorteados > maiorValorEncontrado){
+    maiorValorEncontrado = numeroSorteados;
+    }
+}
+
+print(maiorValorEncontrado);
+
+const quantidadeDeAlunos = gets();
+let maiorValorEncontrado1 = 0;
+
+for (let i = 0; i < quantidadeDeAlunos; i++){
+    const numeroSorteados = gets();
+    if(numeroSorteados > maiorValorEncontrado1){
+    maiorValorEncontrado1 = numeroSorteados;
+    }
+}
+
+print(maiorValorEncontrado1);
